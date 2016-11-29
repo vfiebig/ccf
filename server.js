b7cb8492e82b1a8fa05e157be3f748738cad1a88
Vicente C. Fiebig <vicentefiebig@gmail.com>
2016-11-29 15:16:57 -0200

    trying start a server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-	  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+
+//Lets require/import the HTTP module
+var http = require('http');
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(8080, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
