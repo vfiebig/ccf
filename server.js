730559915c0a225b087051b2d6c026555f92458c
Vicente C. Fiebig <vicentefiebig@gmail.com>
2016-11-29 15:17:11 -0200

    trying start a server

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(8080, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://localhost:%s", 8080);
 });
